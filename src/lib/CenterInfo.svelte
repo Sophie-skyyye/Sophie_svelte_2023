35813c6737418829301aeb695dba9b7a59a66dc4
Sophie Dickerson (1000115856) <DICKSS3@student.op.ac.nz>
2023-08-25 11:27:25 +1200

    added info for the companys and fixed postioning using Using absolute positioning and CSS transforms

---
+++ src/lib/CenterInfo.svelte
@@ -0,0 +1,162 @@
+<div class="Honda">
+    <h1>
+        Honda
+    </h1>
+        <h4> 
+            Heres a little info about Honda
+        </h4>
+        
+        <h4> 
+            Founded:
+        </h4>Hamamatsu, Japan (October 1946, incorporated 24 September 1948; 74 years ago)
+        
+        <h4> 
+            Founder:
+        </h4> Soichiro Honda   
+        
+        <h4> 
+            Headquarters:
+        </h4> Minato, Tokyo, Japan    
+        
+        <h4> 
+            Area served:
+        </h4> Worldwide  
+        
+        <h4> 
+            Key people: 
+        </h4>	
+        
+        <p> Seiji Kuraishi (Chairman)</p>           
+        <p> Toshihiro Mibe (President​ and CEO)</p>            
+        
+        <h4> 
+            Products:
+        </h4>	
+                Automobiles,
+                Commercial vehicles,
+                Luxury cars,
+                Motorcycles,
+                Scooters,
+                Electric generators,
+                Water pumps,
+                Lawn and garden equipment,
+                Rotary tillers,
+                Outboard motors,
+                Robotics,
+                Jet aircraft,
+                Jet engines,
+                Thin-film solar cells,
+                Internavi (telematics)
+    
+</div>
+
+<div class="Subaru">
+ <h1>Subaru </h1>
+    <h4>
+        Founded:	
+    </h4>15 July 1953; 70 years ago[1][2]
+
+    <h4>
+        Founder:	
+    </h4>Kenji Kita, Chikuhei Nakajima
+
+    <h4>
+        Headquarters:	
+    </h4>Ebisu, Shibuya, Japan
+
+    <h4>
+        Area served:	
+    </h4>Worldwide
+
+    <h4>
+        Key people:	
+    </h4>
+        <p>Atsushi Osaki (President, Representative Director & CEO)</p>
+        <p>Tomomi Nakamura (Chairman & Director)</p>
+
+    <h4>
+        Products:	
+    </h4>Automobiles 
+
+</div>
+
+<div class="Toyota">
+    <h1>Toyota </h1>
+    <h4>
+        Founded:	
+    </h4>August 28, 1937; 85 years ago
+
+    <h4>
+        Founder:	
+    </h4>Kiichiro Toyoda
+
+    <h4>
+        Headquarters:	
+    </h4>Toyota City, Aichi, Japan
+
+    <h4>
+        Area served:	
+    </h4>Worldwide
+
+    <h4>
+        Key people:	
+    </h4>
+        <p>	
+            Akio Toyoda (Chairman)
+           </p>
+        <p> 
+            Shigeru Hayakawa (Vice Chairman)
+        </p>
+        <p> 
+            Koji Sato (President & CEO)
+        </p>
+        <p> 
+            Simon Humphries (Chief Designer) 
+        </p>  
+
+    <h4>
+        Products:	
+    </h4>	Automotive
+    <h4>
+        Divisions:
+    </h4>
+    <p>Lexus</p>
+    <p>Scion</p>
+</div>
+
+<div class="Mitsubishi">
+    <h1>Mitsubishi </h1>
+    <h4>
+        Founded:	
+    </h4>22 April 1970; 53 years ago
+
+    <h4>
+        Founder:	
+    </h4>Yataro Iwasaki
+
+    <h4>
+        Headquarters:	
+    </h4>	3-1-1 Shibaura, Minato, Tokyo, Japan
+
+    <h4>
+        Area served:	
+    </h4>Worldwide
+
+    <h4>
+        Key people:	
+    </h4>
+        <p>	
+            Takao Kato (President & CEO)
+        </p>
+        <p>
+            Tomofumi Hiraku(Chairman)
+        </p>
+
+    <h4>
+        Products:	
+    </h4>	
+    Passenger cars,
+    Economy cars,
+    Commercial vehicles,
+    Sport utility vehicles
+</div>
